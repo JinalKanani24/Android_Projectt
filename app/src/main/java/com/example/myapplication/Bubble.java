40fa5cd6b5608332d8adf64f65bbe463886e1c53
JinalKanani24 <jinalkanani24@gmail.com>
2026-01-16 18:52:53 +0530

    bubble.java

---
+++ app/src/main/java/com/example/myapplication/Bubble.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+class Bubble {
+    float x, y, radius;
+    int color;
+    boolean isVisible = true;
+
+    Bubble(float x, float y, float r, int c) {
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+        this.color = c;
+    }
+}
